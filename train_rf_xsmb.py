88a696d9148c2fea8c8e0dba78a3b7911ee0f36f
anhtuluke79 <tutruong19790519@gmail.com>
2025-07-14 14:09:38 +0700

    Update train_rf_xsmb.py

--- train_rf_xsmb.py
+++ train_rf_xsmb.py
@@ -5,11 +5,11 @@ import joblib
 # Đọc dữ liệu, yêu cầu file xsmb.csv đã có các trường: date, DB, G1
 df = pd.read_csv("xsmb.csv")
 
-# Lấy đặc trưng đơn giản: dùng 3 ngày gần nhất trước để dự đoán DB ngày sau (2 số cuối)
+# Lấy đặc trưng đơn giản: dùng 10 ngày gần nhất trước để dự đoán DB ngày sau (2 số cuối)
 X, y = [], []
-for i in range(3, len(df)):
+for i in range(10, len(df)):
     features = []
-    for j in range(i-3, i):
+    for j in range(i-10, i):
         features += [int(str(df.iloc[j]['DB'])[-2:]), int(str(df.iloc[j]['G1'])[-2:])]
     X.append(features)
     y.append(int(str(df.iloc[i]['DB'])[-2:]))
