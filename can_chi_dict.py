cb885f1bc5613a39ccb3a9f04fdeeac89b1447bd
anhtuluke79 <tutruong19790519@gmail.com>
2025-07-15 20:37:05 +0700

    Update can_chi_hanh_dict.py

--- can_chi_dict.py
+++ can_chi_dict.py
@@ -1,3 +1,15 @@
+CAN_INFO = {
+    "Giáp": {"am_duong": "Dương", "ngu_hanh": "Mộc"},
+    "Ất":   {"am_duong": "Âm",   "ngu_hanh": "Mộc"},
+    "Bính": {"am_duong": "Dương", "ngu_hanh": "Hỏa"},
+    "Đinh": {"am_duong": "Âm",   "ngu_hanh": "Hỏa"},
+    "Mậu":  {"am_duong": "Dương", "ngu_hanh": "Thổ"},
+    "Kỷ":   {"am_duong": "Âm",   "ngu_hanh": "Thổ"},
+    "Canh": {"am_duong": "Dương", "ngu_hanh": "Kim"},
+    "Tân":  {"am_duong": "Âm",   "ngu_hanh": "Kim"},
+    "Nhâm": {"am_duong": "Dương", "ngu_hanh": "Thủy"},
+    "Quý":  {"am_duong": "Âm",   "ngu_hanh": "Thủy"},
+}
 data = {
     "Giáp Tý": "4-9,4",
     "Ất Sửu": "5-4,9",
