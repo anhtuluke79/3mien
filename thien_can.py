55dd279b7de834bec59ca50809f6ba061e02a463
anhtuluke79 <tutruong19790519@gmail.com>
2025-07-15 21:17:55 +0700

    Create thien_can.py

---
+++ thien_can.py
@@ -0,0 +1,12 @@
+CAN_INFO = {
+    "Giáp": {"am_duong": "Dương", "ngu_hanh": "Mộc"},
+    "Ất":   {"am_duong": "Âm",   "ngu_hanh": "Mộc"},
+    "Bính": {"am_duong": "Dương", "ngu_hanh": "Hỏa"},
+    "Đinh": {"am_duong": "Âm",   "ngu_hanh": "Hỏa"},
+    "Mậu":  {"am_duong": "Dương", "ngu_hanh": "Thổ"},
+    "Kỷ":   {"am_duong": "Âm",   "ngu_hanh": "Thổ"},
+    "Canh": {"am_duong": "Dương", "ngu_hanh": "Kim"},
+    "Tân":  {"am_duong": "Âm",   "ngu_hanh": "Kim"},
+    "Nhâm": {"am_duong": "Dương", "ngu_hanh": "Thủy"},
+    "Quý":  {"am_duong": "Âm",   "ngu_hanh": "Thủy"},
+}
