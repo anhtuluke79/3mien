fe5706cd67ca2578a85d4d9d15bdd6e0254e03a1
anhtuluke79 <tutruong19790519@gmail.com>
2025-07-15 11:08:53 +0700

    Update menu_handlers.py

--- menu_handlers.py
+++ menu_handlers.py
@@ -100,8 +100,9 @@ async def menu_callback_handler(update: Update, context: ContextTypes.DEFAULT_TY
 
     if data == "pt_theongay":
         context.user_data["mode"] = "phongthuy"
-        await query.edit_message_text("Nhập ngày (YYYY-MM-DD) hoặc can chi (VD: Giáp Tý):")
+        await query.edit_message_text("Nhập ngày tra cứu (vd: 15-07-2025, 15-07 hoặc 15/7/2025, 15/7):")
         return
+
 
     # --- CHỐT SỐ SUBMENU ---
     if data == "submenu_chotso":
