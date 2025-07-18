4b8c6d187310e267fac9564fd8d166ec98b4e20d
anhtuluke79 <tutruong19790519@gmail.com>
2025-07-18 11:41:22 +0700

    Update __init__.py

--- utils/ai_rf/__init__.py
+++ utils/ai_rf/__init__.py
@@ -1,4 +1,9 @@
 # utils/ai_rf/__init__.py
 
-from .rf_lo import train_rf_lo_mb, predict_rf_lo_mb
 from .rf_db import train_rf_db, predict_rf_xsmb
+from .rf_lo import train_rf_lo_mb, predict_rf_lo_mb
+
+def train_all_ai():
+    ok_db = train_rf_db()
+    ok_lo = train_rf_lo_mb()
+    return ok_db and ok_lo
